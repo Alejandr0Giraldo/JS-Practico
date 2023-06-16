d3be07f3fff0a61894cc78e1156872974c91a841
Diego Giraldo <dgiraldogaleano@gmail.com>
2023-06-15 20:50:26 -0500

    Event listener

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const h1 = document.querySelector("h1")
-const p = document.querySelector("p")
-const parrafito = document.querySelector(".parrafito")
-const pid = document.querySelector("#pid")
-const input = document.querySelector("input")
+const input1 = document.querySelector("#calculo1")
+const input2 = document.querySelector("#calculo2")
+const btn = document.querySelector("#btnCalcular")
+const pResult = document.querySelector("#result")
 
-console.log(h1)
+function btnOnClick() {
+   const sumaInputs = input1.value + input2.value
+    pResult.innerText = "Resultado: " + sumaInputs
+}
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-})
